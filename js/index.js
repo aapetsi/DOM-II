f550a9829a3a0d5df897d9a69a24ed670168ce6f
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-09-03 11:18:01 +0000

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+// mouseover event
+
+
+// [ ] keydown
+// [ ] wheel
+// [ ] drag / drop
+// [ ] load
+// [ ] focus
+// [ ] resize
+// [ ] scroll
+// [ ] select
+// [ ] dblclick
